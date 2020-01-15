fb4fc9f1484b99f8891cf82bc8c80bbc14509c1a
Chong Ren <leechongren@users.noreply.github.com>
2020-01-15 17:48:53 +0800

    added checkbox with strikethrough

--- app.js
+++ app.js
@@ -4,19 +4,39 @@ const addButton = document.querySelector("button")
 //when add button clicked, get input value
 addButton.addEventListener("click",createToDoListItem)
 
-
+function removeItem(){
+    event.target.parentElement.remove()
+}
 
 function createToDoListItem(){
     
-    const input = document.querySelector("input[type=text]")
+    const input = document.querySelector("input[type=text]")    
     //check if input is empty
     if (!input.value){
         return;
     }
     //create an <li> element
     const li = document.createElement("li")
-    //add text to li element
-    li.textContent = input.value  //need .value to parse it into a string
+
+
+    // adding delete button
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "❌"
+
+    deleteButton.addEventListener("click",removeItem)
+
+    //creating p tag with the input
+    const pTagInput = createPElement(input.value)
+
+    //adding a checkbox
+    const checkbox = createCheckBox()
+    checkbox.addEventListener('change',toggleComplete)
+
+
+    li.appendChild(checkbox)
+    li.appendChild(pTagInput)
+    li.appendChild(deleteButton)
+
     //find the ul element and 
     const ul = document.querySelector("ul")
     //append li element to ul element
@@ -25,4 +45,29 @@ function createToDoListItem(){
     //resets the value to blank string
     input.value = ""
 }
+
+function toggleComplete(event){
+    console.log(event.target.nextSibling)
+    event.target.nextSibling.classList.toggle("todo")
+    event.target.nextSibling.classList.toggle("done")
+}
+
+
+function createCheckBox(){
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    return checkbox
+}
+
+//function to create a p tag
+function createPElement(content){
+    const pItem = document.createElement("p")
+    pItem.classList.add("todo")
+    pItem.textContent = content
+    // pItem.addEventListener("click",toggleComplete)
+
+    return pItem
+
+}
+
 
